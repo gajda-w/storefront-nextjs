276aff9ad71d0943085846ff85e7c0ad6cffda3f
Wojciech Gajda <wojciech.gajda@mirumee.com>
2024-03-09 18:01:29 +0100

    add variant selector

--- config.ts
+++ config.ts
@@ -1,1 +1,17 @@
+import resolveConfig from "tailwindcss/resolveConfig";
+
+import twConfig from "./tailwind.config";
+
 export const DEFAULT_DEBOUNCE_TIME_IN_MS = 500;
+
+export const config = resolveConfig(twConfig);
+
+type ScreenSize = keyof typeof config.theme.screens;
+export const screenSizes = Object.entries(config.theme.screens).reduce(
+  (acc, [breakpoint, size]) => {
+    acc[breakpoint as ScreenSize] = Number(size.match(/\d+/g)?.[0]);
+
+    return acc;
+  },
+  {} as Record<ScreenSize, number>,
+);
--- src/components/variant-dropdown.tsx
+++ src/components/variant-dropdown.tsx
@@ -1,25 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
-import { SelectContent, SelectItem, SelectTrigger } from "@radix-ui/react-select";
-import { Select } from "@/components/ui/select";
+import { useEffect, useState } from "react";
+import { CheckIcon } from "lucide-react";
+import { useWindowSize } from "usehooks-ts";
+import { usePathname, useRouter } from "next/navigation";
+import { screenSizes } from "../../config";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { type VariantFragment } from "@/gql/graphql";
 import { type Maybe } from "@/lib/types";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Label } from "@/components/ui/label";
 
 type VariantDropdownProps = {
   variants: Maybe<VariantFragment[]>;
 };
 
+const getIdFromHash = () => (typeof window === "undefined" ? "" : location.hash.replace("#", ""));
+
 export const VariantDropdown = ({ variants }: VariantDropdownProps) => {
+  const [selectedVariant, setSelectedVariant] = useState("");
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const { width } = useWindowSize();
+  const isSmDown = width && width < screenSizes.sm;
+
+  useEffect(() => {
+    if (selectedVariant) {
+      router.push(`${pathname}#${selectedVariant}`, { scroll: false });
+    } else if (getIdFromHash() && !selectedVariant) {
+      router.push(pathname, { scroll: false });
+    }
+  }, [selectedVariant]);
+
+  useEffect(() => {
+    /**
+     * Cant set this in initial useState due hydration error,
+     */
+    const variantId = getIdFromHash();
+
+    if (variantId) {
+      setSelectedVariant(variantId);
+    }
+  }, []);
+
   return (
-    <Select>
-      <SelectTrigger>Select Variant</SelectTrigger>
-      <SelectContent>
-        {variants?.map((variant) => (
-          <SelectItem value={variant.id} key={variant.id}>
-            {variant.name}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <>
+      <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+        <SheetContent side="bottom">
+          <ul>
+            {variants?.map(({ id, ...variant }) => (
+              <li
+                key={id}
+                className="flex cursor-pointer"
+                onClick={() => {
+                  setSelectedVariant(id);
+                  setIsSheetOpen(false);
+                }}
+              >
+                <Button variant="ghost" className="w-full justify-start p-1.5">
+                  <CheckIcon
+                    className={cn("invisible mr-2 h-auto w-[20px]", {
+                      visible: selectedVariant === id,
+                    })}
+                  />
+                  {variant.name}
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </SheetContent>
+      </Sheet>
+
+      <Label>Select variant</Label>
+      <Select
+        disabled={!variants?.length}
+        open={isSmDown ? false : undefined}
+        onValueChange={(variantId) => setSelectedVariant(variantId)}
+        onOpenChange={() => isSmDown && setIsSheetOpen(true)}
+        value={variants?.length && selectedVariant ? selectedVariant : undefined}
+      >
+        <SelectTrigger className="mt-1">
+          <SelectValue placeholder="Select variant" />
+        </SelectTrigger>
+        <SelectContent>
+          {variants?.map(({ id, name }) => (
+            <SelectItem key={id} value={id}>
+              {name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </>
   );
 };
 
